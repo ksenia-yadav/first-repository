28bf88681c138ef63ddafabac33635fb3af77258
Ksenia Yadav <ksenia.yadav@gmail.com>
2015-04-29 11:26:04 -0400

    A first commit from Mac to the repository.

--- first_testfile.py
+++ first_testfile.py
@@ -1,3 +1,9 @@
 '''
 first_testfile.py
 '''
+
+print('To see a world in a grain of sand')
+print('And a heaven in a wild flower')
+print('Hold infinity in the palm of your hand')
+print('And eternity in an hour')
+
